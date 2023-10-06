0e657beb224eead59b042e5cec65ee72f3d83731
faustojc <61080389+faustojc@users.noreply.github.com>
2023-10-06 14:29:31 +0800

    Update main.py

--- thesis/main.py
+++ thesis/main.py
@@ -1,6 +1,9 @@
 import re
 import pandas as pd
 
-df = pd.read_csv('data/Thesis Data.csv')
+df = pd.read_csv('data/Thesis Data.csv', skiprows=1, encoding='latin-1')
+
+df['STREET NAME/SUBD/CONDO'] = df['STREET NAME/SUBD/CONDO'].ffill()
+df['VICINITY'] = df['VICINITY'].ffill()
 
 
