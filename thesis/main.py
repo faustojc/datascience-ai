e719bad582c3bf86d74d8e1dde047fdca029036f
faustojc <61080389+faustojc@users.noreply.github.com>
2023-10-07 16:28:38 +0800

    Delete main.py

--- thesis/main.py
+++
@@ -1,9 +0,0 @@
-import re
-import pandas as pd
-
-df = pd.read_csv('data/Thesis Data.csv', skiprows=1, encoding='latin-1')
-
-df['STREET NAME/SUBD/CONDO'] = df['STREET NAME/SUBD/CONDO'].ffill()
-df['VICINITY'] = df['VICINITY'].ffill()
-
-
