cb15769a911668e9a193c157c87e0c72da99c3c2
faustojc <faustojcboko@gmail.com>
2023-10-27 19:14:03 +0800

    Reformat and simplified

--- connect-4/main.py
+++ connect-4/main.py
@@ -10,6 +10,7 @@ board = [
 player = 'X'
 movesLeft = 42
 
+
 def print_board():
     for row in board:
         for col in row:
@@ -22,40 +23,43 @@ def print_board():
         else:
             print(end='\n')
 
+
 def make_move(move, letter):
     for row in range(6, 0, -1):
         if board[1][move] != '_':
             print('Column Occupied!')
             return False
-        
+
         if board[row][move] == '_':
             board[row][move] = letter
             return True
-        
+
     return False
+
 
 def winner(letter):
     # check all rows
     for row in range(1, 7):
         for col in range(1, 5):
-            if board[row][col] == board[row][col+1] == board[row][col+2] == board[row][col+3] == letter:
+            if board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] == letter:
                 return True
 
     # check all columns
     for col in range(1, 8):
         for row in range(1, 4):
-            if board[row][col] == board[row+1][col] == board[row+2][col] == board[row+3][col] == letter:
+            if board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] == letter:
                 return True
 
     # check diagonals
     for row in range(1, 4):
         for col in range(1, 5):
-            if board[row][col] == board[row+1][col+1] == board[row+2][col+2] == board[row+3][col+3] == letter:
+            if board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] == letter:
                 return True
-            if board[row][col+3] == board[row+1][col+2] == board[row+2][col+1] == board[row+3][col] == letter:
+            if board[row][col + 3] == board[row + 1][col + 2] == board[row + 2][col + 1] == board[row + 3][col] == letter:
                 return True
 
     return False
+
 
 def start():
     global player, movesLeft
@@ -69,12 +73,12 @@ def start():
 
         try:
             move = int(input(f'Player {player} col (1 to 7): '))
-            
-            if (move <= 0 and move >= 8):
+
+            if 0 >= move >= 8:
                 print('Invalid move!')
-                continue   
-            
-            # Player's turn
+                continue
+
+                # Player's turn
             if make_move(move, player):
                 print_board()
 
@@ -84,9 +88,9 @@ def start():
 
                 player = 'X' if player == 'O' else 'O'
                 movesLeft -= 1
-        except:
-            print('Invalid input!')      
-            
+        except ValueError:
+            print('Invalid input!')
+
 
 if __name__ == '__main__':
     start()
