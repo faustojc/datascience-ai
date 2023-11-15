cc44fff6e2445e3ff87c65887b3759118f25b8a4
faustojc <faustojcboko@gmail.com>
2023-11-15 20:47:15 +0800

    Refactor

--- ai/binaryTree.py
+++ ai/binaryTree.py
@@ -1,23 +1,21 @@
 def generate_tree(arr):
     if not arr:
         return None
-    
+
     mid = len(arr) // 2
-    root = {'val': arr[mid], 'left': None, 'right': None}
-    
-    root['left'] = generate_tree(arr[:mid])
-    root['right'] = generate_tree(arr[mid+1:])
-    
+    root = {'val': arr[mid], 'left': generate_tree(arr[:mid]), 'right': generate_tree(arr[mid + 1:])}
+
     return root
+
 
 def print_tree(root, level=0):
     if root:
-        print_tree(root['right'], level+1)
+        print_tree(root['right'], level + 1)
         print(' ' * 4 * level + '->', root['val'])
-        print_tree(root['left'], level+1)
+        print_tree(root['left'], level + 1)
+
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-root = generate_tree(arr)
+tree = generate_tree(arr)
 
-print_tree(root)
-
+print_tree(tree)
