20c44c007b97fe0329bce606e1fa31260222aac6
faustojc <faustojcboko@gmail.com>
2023-11-28 16:58:04 +0800

    Update cpu tictactoe

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,13 +1,74 @@
-from computer import Computer
-
 board = [
     [' ', '1', '2', '3'],
     ['1', '_', '_', '_'],
     ['2', '_', '_', '_'],
     ['3', '_', '_', '_']
 ]
-player = 'X'
-movesLeft = 9
+
+class Computer:
+    def __init__(self, team):
+        self.team = team
+        self.best_move = [-1, -1]
+
+
+    @staticmethod
+    def empty_cells(board: list[list[str]]):
+        cells = [[row, col] for row in range(1, 4) for col in range(1, 4) if board[row][col] == '_']
+
+        return cells
+
+
+    def __minimax(self, board: list[list[str]], depth: int, isMaximizing: bool) -> int:
+        opponent = 'O' if self.team == 'X' else 'X'
+        bestScore = -1000 if isMaximizing else 1000
+
+        # Scoring system
+        if winner(self.team):
+            return 1
+        elif winner(opponent):
+            return -1
+        elif len(self.empty_cells(board)) == 0:
+            return 0
+
+        if isMaximizing:
+            for cell in self.empty_cells(board):
+                board[cell[0]][cell[1]] = self.team
+                score = self.minimax(board, depth + 1, False)
+
+                board[cell[0]][cell[1]] = '_'
+                bestScore = max(score, bestScore)
+
+            return bestScore
+        else:
+            for cell in self.empty_cells(board):
+                board[cell[0]][cell[1]] = opponent
+                score = self.minimax(board, depth + 1, True)
+
+                board[cell[0]][cell[1]] = '_'
+                bestScore = min(score, bestScore)
+
+        return bestScore
+
+    def __find_best_move(self, board: list[list[str]]) -> None:
+        bestScore = -1000
+
+        for cell in self.empty_cells(board):
+            board[cell[0]][cell[1]] = self.team
+
+            score = self.minimax(board, 0, False)
+
+            board[cell[0]][cell[1]] = '_'
+
+            if score > bestScore:
+                bestScore = score
+                self.best_move = cell
+
+    def make_move(self, board: list[list[str]]) -> bool:
+        self.find_best_move(board)
+
+        board[self.bestMove[0]][self.bestMove[1]] = self.team
+
+        return self.bestMove[0] != self.bestMove[1] != -1
 
 
 def print_board():
@@ -55,8 +116,9 @@ def winner(letter):
 
 
 def start():
-    global player, movesLeft
-    computer = Computer('O', 'minimax')
+    player = 'X'
+    movesLeft = 9
+    computer = Computer('O')
 
     print_board()
     print("Input format: row, col (e.g. 1,2)", end='\n\n')
@@ -83,22 +145,20 @@ def start():
                 print(f'Player {player} wins!')
                 break
 
-            # player = 'X' if player == 'O' else 'O'
             movesLeft -= 1
-
-            # Computer's turn
-            if computer.make_move(board):
-                print(f'Computer moves ({computer.bestMove[0]}, {computer.bestMove[1]})')
-                print_board()
-
-                if winner(computer.team):
-                    print(f'Computer {computer.team} wins!')
-                    break
-
-                movesLeft -= 1
         else:
             print('Invalid move. Try again.')
 
+        # Computer's turn
+        if computer.make_move(board):
+            print(f'Computer moves ({computer.best_move[0]}, {computer.best_move[1]})')
+            print_board()
+
+            if winner(computer.team):
+                print(f'Computer {computer.team} wins!')
+                break
+
+            movesLeft -= 1
 
 if __name__ == '__main__':
     start()
